422d4ee6543d3fcb0e0bb33f02d13055f42fd6e1
laojuaihe22 <jylee073@mymail.sim.edu.sg>
2024-10-07 12:37:57 +0800

    setup tailwindcss for merchant

--- shiokority_merch/src/index.jsx
+++ shiokority_merch/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals.js';
 import App from './App.jsx'
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
