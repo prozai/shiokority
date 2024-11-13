114a9929efc3c5a8b576450011c6f81a8fbc3265
laojuaihe22 <jylee073@mymail.sim.edu.sg>
2024-11-13 16:43:53 +0800

    adjustment on databaseConnection

--- shiokorityAPI/app/auth/databaseConnection.py
+++ shiokorityAPI/app/auth/databaseConnection.py
@@ -1,16 +1,16 @@
-from flask import current_app
+from config import config
 import pymysql
 
 def getDBConnection(schema_name):
-    config = {
-        'host': current_app.config['MYSQL_HOST'],
-        'user': current_app.config['MYSQL_USER'],
-        'password': current_app.config['MYSQL_PASSWORD'],
+    configuration = {
+        'host': config['development'].MYSQL_HOST,
+        'user': config['development'].MYSQL_USER,
+        'password': config['development'].MYSQL_PASSWORD,
         'database': schema_name,
         'cursorclass': pymysql.cursors.DictCursor
     }
         # Create the connection
-    connection = pymysql.connect(**config)
+    connection = pymysql.connect(**configuration)
     
     # Set timezone for this connection
     with connection.cursor() as cursor:
